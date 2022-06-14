b85447683e28b21731a35147f4cccc992cb43e00
Alfonso <alcapecar01@gmail.com>
2022-06-14 20:01:42 +0200

    Filtro parte 1

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,7 @@ Route::delete('/users/{id}',[UserController::class,'destroy']);
 Route::post('/users/{email}',[UserController::class,'getByEmail']);
 Route::get('/entrenadores/all',[UserController::class,'getEntrenadores']);
 Route::get('/jugadores/all',[UserController::class,'getJugadores']);
+Route::post('/users/filtrado', [UserController::class,'postFiltrado']);
 
 Route::get('/solicitudes/all',[SolicitudController::class,'getAll']);
 Route::get('/solicitudes/misequipos/{id}',[SolicitudController::class,'misEquipos']);
